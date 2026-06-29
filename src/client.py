06722a4244975940f3538945c1a925b9d41db761
Maciej Krzywicki <maciastydev@gmail.com>
2026-06-29 10:35:42 +0200

    implement ACLED OAuth client with pydantic-settings config
    Closes #2

---
+++ src/client.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+
+import requests
+import logging
+from src.settings import Settings
+
+logger = logging.getLogger(__name__)
+
+@dataclass(frozen=True, slots=True)
+class Token:
+    access_token: str
+    refresh_token: str
+
+
+class ACLEDClient:
+
+    def __init__(self):
+        self.settings = Settings()
+        self._session = requests.Session()
+
+    def get_token(self):
+        response = self._session.post(
+            self.settings.auth_url,
+            data= {
+                "username": self.settings.email,
+                "password": self.settings.password,
+                "grant_type": "password",
+                "client_id": "acled",
+                "scope": "authenticated",
+            }
+        )
+        response.raise_for_status()
+        data = response.json()
+        self._token = Token(
+            access_token=data["access_token"],
+            refresh_token=data["refresh_token"],
+        )
+        logger.info(f"auth successful{self._token.access_token[:10]}")
